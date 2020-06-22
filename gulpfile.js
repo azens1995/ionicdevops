e2e249e2c1c6b692d4c34e89c58add2dceed7cf6
Eklak Dangaura <azens1995@gmail.com>
2020-06-22 10:19:27 +0545

    Add APK signing and align

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,19 @@ const { execSync } = require('child_process');
 
 gulp.task('prod-android', async function() {
     await addRemovePlatform('android');
-    await build('android', 'prod');
+    return setTimeout(async function () {
+        await build('android', 'prod', true);
+        await sign ('prod');
+        await align('prod');
+        return;
+    }, 5000);
 });
 
 async function addRemovePlatform(platform) {
     console.log(`Check if ${platform} platform already exists.....`);
-    // const platformPath = path.join(cordova.findProjectRoot(), 'platforms', platform);
-    // const exists = fs.existsSync(platformPath);
-    if(cordova.findProjectRoot()){
+    const platformPath = path.join(cordova.findProjectRoot(), 'platforms', platform);
+    const exists = fs.existsSync(platformPath);
+    if(exists){
         console.log(`${platform} platform already exists, remove ${platform} platfrom....`);
         await cordova.platform('remove', platform);
         console.log(`${platform} platform successfully removed....`);
@@ -37,3 +42,45 @@ async function build(platform, env, isRelease = true){
     console.log(`Sucessfully finished build for ${platform} :)`);
 }
 
+// apk signing for android
+async function sign(env){
+    console.log('Start apk signing....');
+    if(env != 'uat'){
+        await execSync('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sampleapp.keystore  platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk -storepass sample sampleapp', {stdio: 'inherit'});
+    }else {
+        await execSync('jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore sampleapp.keystore  platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk -storepass sample sampleapp', {stdio: 'inherit'});
+    }
+    
+    console.log('Finsihed signing apk....')
+}
+
+// zip-align for android
+async function align(env){
+    
+    console.log('Finding build-tool version...')
+    const buildToolVersionString = execSync('ls $ANDROID_HOME/build-tools/',  { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 }).toString();
+    
+    if(!buildToolVersionString){
+        console.error('Could not find android sdk :\'(');
+    }
+    
+    const buildToolVersions = buildToolVersionString.split('\n');
+    const buildToolVersionToUse = buildToolVersions[0];
+    
+    console.log(`Using android sdk version ${buildToolVersionToUse}...`);
+    const androidHomePath = execSync('echo $ANDROID_HOME',  {encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 }).toString().split('\n')[0];
+    
+    console.log(`Check if tribute-${env}.apk already exists...`);
+    
+    const exists = fs.existsSync(`./tribute-${env}.apk`);
+    
+    if(exists){
+        console.log(`tribute-${env}.apk already exists, delete it`);
+        await fs.unlinkSync(`tribute-${env}.apk`);
+        console.log(`Finished deleting tribute-${env}.apk`);
+    }
+    console.log('Start zip align...')
+    await execSync(`${androidHomePath}/build-tools/${buildToolVersionToUse}/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk tribute-${env}.apk`, {stdio: 'inherit'})
+    
+    console.log('Finished zipalign...')
+}
